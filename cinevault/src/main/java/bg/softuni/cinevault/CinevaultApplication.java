bdb0f0bb183eeab226ecde11b7bf05ce862fa296
Ralitsa Dzhukeva <ralitsadzhukeva@gmail.com>
2026-06-12 12:49:29 +0300

    git init git add . git commit -m "chore: initialize spring boot project"

---
+++ cinevault/src/main/java/bg/softuni/cinevault/CinevaultApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.cinevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinevaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinevaultApplication.class, args);
+	}
+
+}
---
+++ cinevault/src/test/java/bg/softuni/cinevault/CinevaultApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.cinevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinevaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
